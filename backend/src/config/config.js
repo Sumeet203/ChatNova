2c20bf8176c5dfdabe3d7403467f96896ab0eac7
Sumeet203 <sumeetkothiyal203@gmail.com>
2026-09-05 23:05:59 +0530

    feat: initialize configuration files for frontend API client and backend environment variables

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3000";
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const BACKEND_URL = process.env.BACKEND_URL;
+export const FRONTEND_URL = process.env.FRONTEND_URL;
 export const PORT = process.env.PORT || 3000;
--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "https://chatnova-dshx.onrender.com";
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
